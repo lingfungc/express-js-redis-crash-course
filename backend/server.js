9ebcc795c379f5a1039ed71eef9ccf1425046e36
Ling Fung Chan <justinlfchan@gmail.com>
2023-08-20 23:52:27 +0100

    npm installed redis and setting up redis for getting all photos with redis-create-client

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,13 @@
 const express = require("express");
 const axios = require("axios");
 const cors = require("cors");
+const Redis = require("redis");
+
+// const client = Redis.createClient({ url: })
+const redisClient = Redis.createClient();
+
+// Set expiration to 1 hour
+const DEFAULT_EXPIRATION = 3600;
 
 const app = express();
 app.use(cors());
@@ -9,6 +16,10 @@ app.get("/photos", async (req, res) => {
   const { data } = await axios.get(
     "https://jsonplaceholder.typicode.com/photos"
   );
+
+  // This "photos" is the key of the key-value pair in our redis here
+  // In redis, we can only store String, so we need to convert the data to String
+  redisClient.setex("photos", DEFAULT_EXPIRATION, JSON.stringify(data));
 
   // const albumId = req.query.albumId;
   // console.log(albumId);
