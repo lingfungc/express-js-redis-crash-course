722f4b0c5cad8b79a3faca19d770933b27adcab1
Ling Fung Chan <justinlfchan@gmail.com>
2023-08-21 08:17:10 +0100

    updated redis-client usage in app.get('photos') to set and get photos-data with redis

--- backend/server.js
+++ backend/server.js
@@ -10,16 +10,49 @@ const redisClient = Redis.createClient();
 const DEFAULT_EXPIRATION = 3600;
 
 const app = express();
+app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
 app.get("/photos", async (req, res) => {
-  const { data } = await axios.get(
-    "https://jsonplaceholder.typicode.com/photos"
-  );
+  redisClient.on("error", (err) => console.log("Redis Client Error:", err));
 
-  // This "photos" is the key of the key-value pair in our redis here
-  // In redis, we can only store String, so we need to convert the data to String
-  redisClient.setex("photos", DEFAULT_EXPIRATION, JSON.stringify(data));
+  await redisClient.connect();
+
+  // redisClient.del("photos");
+
+  const photosData = await redisClient.get("photos");
+  console.log(`This is photoData: ${photosData}`);
+
+  if (photosData != null) {
+    console.log("Cache hit");
+    res.json(JSON.parse(photosData));
+  } else {
+    const { data } = await axios.get(
+      "https://jsonplaceholder.typicode.com/photos"
+    );
+    // This "photos" is the key of the key-value pair in our redis here
+    // In redis, we can only store String, so we need to convert the data to String
+    redisClient.set("photos", JSON.stringify(data));
+    redisClient.expire("photos", DEFAULT_EXPIRATION);
+    res.json(data);
+  }
+
+  await redisClient.quit();
+
+  // redisClient.get("photos", async (error, photos) => {
+  //   if (error) console.error(error);
+  //   if (photos != null) {
+  //     console.log("Cache hit");
+  //     return res.json(JSON.parse(photos));
+  //   } else {
+  //     const { data } = await axios.get(
+  //       "https://jsonplaceholder.typicode.com/photos"
+  //     );
+  //     // This "photos" is the key of the key-value pair in our redis here
+  //     // In redis, we can only store String, so we need to convert the data to String
+  //     redisClient.setex("photos", DEFAULT_EXPIRATION, JSON.stringify(data));
+  //   }
+  //   res.json(data);
 
   // const albumId = req.query.albumId;
   // console.log(albumId);
@@ -28,8 +61,6 @@ app.get("/photos", async (req, res) => {
   //   "https://jsonplaceholder.typicode.com/photos",
   //   { params: { albumId } }
   // );
-
-  res.json(data);
 });
 
 app.get("/photos/:id", async (req, res) => {
@@ -40,4 +71,5 @@ app.get("/photos/:id", async (req, res) => {
   res.json(data);
 });
 
+// URL reference: http://localhost:3000/photos
 app.listen(3000, () => console.log("Server is listening to port 3000"));
